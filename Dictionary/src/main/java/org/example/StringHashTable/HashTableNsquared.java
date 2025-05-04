fcb9e18ac800cc935421867fecfaeadf7a3bef18
Michael Magdy <michaelmagdymagdy@gmail.com>
2025-05-05 01:36:24 +0300

    implementation classes

--- Dictionary/src/main/java/org/example/StringHashTable/HashTableNsquared.java
+++ Dictionary/src/main/java/org/example/StringHashTable/HashTableNsquared.java
@@ -1,5 +1,31 @@
 package org.example.StringHashTable;
 
-public class HashTableNsquared {
+import java.util.ArrayList;
+import java.util.List;
+
+public class HashTableNsquared implements HashTableInterface {
+    private PrimeGenerator primeGenerator;
+    List<String> table = new ArrayList<>();
+
+    public HashTableNsquared(PrimeGenerator primeGenerator){
+        this.primeGenerator = primeGenerator;
+    }
+    @Override
+    public Boolean search(String str) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'search'");
+    }
+
+    @Override
+    public void insert(String str) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+
+    @Override
+    public void delete(String str) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
     
 }
--- Dictionary/src/main/java/org/example/StringHashTable/HashTableTwoLevel.java
+++ Dictionary/src/main/java/org/example/StringHashTable/HashTableTwoLevel.java
@@ -1,5 +1,23 @@
 package org.example.StringHashTable;
 
-public class HashTableTwoLevel {
+public class HashTableTwoLevel implements HashTableInterface{
+
+    @Override
+    public Boolean search(String str) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'search'");
+    }
+
+    @Override
+    public void insert(String str) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+
+    @Override
+    public void delete(String str) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
     
 }
